9679b5512e99cc0383cfadb483028bf40632f758
Abdul <AMalik@lcs.on.ca>
2021-12-10 09:06:17 -0500

    I finished creating my listOfQuiz! I made my questions and gave some answers.

--- KinesiologyQuiz/Model/Quiz.swift
+++ KinesiologyQuiz/Model/Quiz.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct Quiz {
+    
+    let answer: String
+    let question: String
+    
+}
+
+let listOfQuiz = [
+
+    Quiz(answer: "Femur", question: "What is the bone in your upper leg?")
+    ,
+    Quiz(answer: "Tibia", question: "What is the bone in the front of your lower leg?")
+    ,
+    Quiz(answer: "Fibula", question: "What is the bone in the back of your lower leg?")
+    ,
+    Quiz(answer: "Radius", question: "What is the bone in your lower arm that is bigger at the wrist?")
+    ,
+    Quiz(answer: "Ulna", question: "What is the bone in your lower arm that is bigger at the elbow?")
+    ,
+    Quiz(answer: "Humerus", question: "What is the bone in your upper arm?")
+    ,
+    Quiz(answer: "Carpols", question: "What are the bones in your wrist?")
+    ,
+    Quiz(answer: "Tarsals", question: "What are the bones that make up your ankle?")
+    ,
+    Quiz(answer: "Phalanges", question: "What are the bones that are in both your toes and fingers?")
+    
+]
