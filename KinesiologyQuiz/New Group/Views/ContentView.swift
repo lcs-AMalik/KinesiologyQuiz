8a08c18c9ebc797ba85aeab6d528ef4f57be9ec7
Abdul <AMalik@lcs.on.ca>
2021-12-10 09:14:43 -0500

    Made KinesiologyQuiz to the same task as FlashCards

--- KinesiologyQuiz/New Group/Views/ContentView.swift
+++ KinesiologyQuiz/New Group/Views/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    
+    //This is the card we are currently quizzing on
+    @State var currentCard: Quiz = listOfQuiz.randomElement()!
+    //This is the prior card that we showed
+    @State var previousCard: Quiz = listOfQuiz.randomElement()!
+    //This controls whether the answer is visible
+    @State var isAnswerShowing = false
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 25) {
+            
+            //Output
+            Text(currentCard.question)
+                .font(.largeTitle)
+            
+            //Input
+            Button(action: {
+                withAnimation {
+                    isAnswerShowing = true
+                }
+                
+            }, label: {
+                Text("Check")
+            })
+                .buttonStyle(.bordered)
+            
+            //Output
+            Text(currentCard.answer)
+                .font(.title)
+            //              CONDITION     True  False
+                .opacity(isAnswerShowing ? 1.0 : 0.0)
+            
+            
+            //Input
+            Button(action: {
+                
+                // previousCard = listOfCards.randomElement()!
+                
+                //Hide the answer
+                //withAnimation {
+                isAnswerShowing = false
+                //}
+                
+                // Pick a new Card
+                currentCard = listOfQuiz.randomElement()!
+                
+                // print("Button was pressed")
+            }, label: {
+                Text("Another")
+            })
+                .buttonStyle(.bordered)
+                .opacity(isAnswerShowing ? 1.0 : 0.0)
+            Spacer()
+            
+        }
+        .navigationTitle("Kinesiology Quiz")
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
+        
     }
 }
