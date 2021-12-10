300c452592dfd589c6c3273bb15d0227813e4024
Abdul <AMalik@lcs.on.ca>
2021-12-10 08:45:40 -0500

    Initial Commit

---
+++ KinesiologyQuiz/KinesiologyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinesiologyQuizApp.swift
+//  KinesiologyQuiz
+//
+//  Created by Abdul Malik on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct KinesiologyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
